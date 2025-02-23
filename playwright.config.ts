c6aa018d40043100a9237354737a68928591a987
Дмитрий Абрамов <daabramov_2@edu.hse.ru>
2025-02-23 19:51:11 +0300

    Тесты входа

---
+++ playwright.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './src',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    trace: 'on-first-retry',
+    testIdAttribute: 'data-test'
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
+});
---
+++ src/login.spec.ts
@@ -0,0 +1,50 @@
+import { test, expect } from '@playwright/test';
+
+const LOGIN = 'standard_user'
+const PASSWORD = 'secret_sauce'
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+});
+
+test.describe('Вход', () => {
+    test('Не даст войти с пустым паролем', async ({ page }) => {
+        await page.getByPlaceholder('Username').fill(LOGIN)
+        await page.getByPlaceholder('Password').fill('')
+        await page.getByRole('button').click()
+
+        const errorTag = page.getByTestId('error')
+        expect(errorTag).not.toBeNull()
+        await expect(errorTag).toHaveText('Epic sadface: Password is required')
+    })
+
+    test('Не даст войти с пустым логином', async ({ page }) => {
+        await page.getByPlaceholder('Username').fill('')
+        await page.getByPlaceholder('Password').fill(PASSWORD)
+        await page.getByRole('button').click()
+
+        const errorTag = page.getByTestId('error')
+        expect(errorTag).not.toBeNull()
+        await expect(errorTag).toHaveText('Epic sadface: Username is required')
+    })
+
+    test('Не даст войти с неправильным паролем', async ({ page }) => {
+        await page.getByPlaceholder('Username').fill(LOGIN)
+        await page.getByPlaceholder('Password').fill('wrong')
+        await page.getByRole('button').click()
+
+        const error = page.getByTestId('error')
+        expect(error).not.toBeNull()
+        await expect(error).toHaveText('Epic sadface: Username and password do not match any user in this service')
+    })
+
+    test('Успешно войдёт, если ввести правильный логин и правильный пароль', async ({ page }) => {
+        await page.getByPlaceholder('Username').fill(LOGIN)
+        await page.getByPlaceholder('Password').fill(PASSWORD)
+        await page.getByRole('button').click()
+
+        const title = page.getByTestId('title')
+        expect(title).not.toBeNull()
+        await expect(title).toBeVisible()
+    })
+})
